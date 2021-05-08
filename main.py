71e5b0474bf8860f48d8876f94b5bc7e88bb79a2
Joao Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2021-05-08 05:40:16 -0300

    Update program

--- main.py
+++ main.py
@@ -1,17 +1,49 @@
-# Author: João Gabriel Cardoso
-# GitHub: github.com/joaogabrielrc
-
 
 import tkinter as tk
 
-# Internal module
-from app.calculator import Calculator
+class Calculator():
+    def __init__(self, window):
+        self.window = window
 
-if __name__ == "__main__":
-    # Creating the window
-    window = tk.Tk()    
 
-    calculator = Calculator(window)
+        # Window settings
+        self.window.title("Calculator")
+        self.window.maxsize(width=350, height=500)
+        self.window.minsize(width=350, height=500)
+        self.window.geometry("+150+100")
+        self.window["bg"] = "#222"
 
-    # running the program
-    calculator.start()
+
+        # Creating the input
+        self.input_frame = tk.Frame()
+        self.input_content()
+
+
+        # Packing the frames
+        self.input_frame.pack()
+        
+    def start():
+        window.mainloop()
+
+    def input_content(self):
+        input_entry = tk.Entry(
+                master=self.input_frame, 
+                font=("Arial", 40), 
+                width=60, 
+                bg="#222", 
+                fg="#fff", 
+                justify="right", 
+                relief="flat",
+                borderwidth=10
+            )
+        input_entry.focus()
+        input_entry.pack()
+
+
+# Creating the window
+window = tk.Tk()
+Calculator(window)
+
+
+# Starting the calculator
+Calculator.start()
