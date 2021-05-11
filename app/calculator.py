752a13d1699db4c7ca707a012698a4cb166bec1d
Joao Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2021-05-10 23:01:18 -0300

    Update app

--- app/calculator.py
+++ app/calculator.py
@@ -14,18 +14,18 @@ class Calculator():
 
         # Window settings
         self.window.title("Calculator")
-        self.window.maxsize(width=320, height=430)
-        self.window.minsize(width=320, height=430)
+        self.window.maxsize(width=290, height=430)
+        self.window.minsize(width=290, height=430)
         self.window.geometry("+150+100")
-        self.window["bg"] = self.settings["global"]["bg"]
+        self.window["bg"] = "#1a1a1a"
 
         # Creating the input
-        self.input_frame = tk.Frame(self.window, cnf=self.settings["global"])
+        self.input_frame = tk.Frame(self.window, bg="#1a1a1a")
         self.input_frame.pack()
         self.create_input_content()
 
         # Creating the buttons
-        self.buttons_frame = tk.Frame(self.window, cnf=self.settings["global"])
+        self.buttons_frame = tk.Frame(self.window, bg="#666666")
         self.buttons_frame.pack()
         self.create_buttons_content()
 
@@ -39,17 +39,42 @@ class Calculator():
         self.entry = tk.Entry(self.input_frame, cnf=self.settings["input"])
         self.entry.focus()
         self.entry.insert(0,0)
-        self.entry.pack()
+        self.entry.pack(padx=10, pady=20)
 
     def create_buttons_content(self):
 
-        # Creating the numbers of calculator
+        # Creating the calculator numbers
+        self.settings["buttons"]["numbers"].update(self.settings["buttons"]["global"])
+
         self.btn_0 = tk.Button(self.buttons_frame, text=0, cnf=self.settings["buttons"]["numbers"])
         self.btn_1 = tk.Button(self.buttons_frame, text=1, cnf=self.settings["buttons"]["numbers"])
+        self.btn_2 = tk.Button(self.buttons_frame, text=2, cnf=self.settings["buttons"]["numbers"])
+        self.btn_3 = tk.Button(self.buttons_frame, text=3, cnf=self.settings["buttons"]["numbers"])
+        self.btn_4 = tk.Button(self.buttons_frame, text=4, cnf=self.settings["buttons"]["numbers"])
+        self.btn_5 = tk.Button(self.buttons_frame, text=5, cnf=self.settings["buttons"]["numbers"])
+        self.btn_6 = tk.Button(self.buttons_frame, text=6, cnf=self.settings["buttons"]["numbers"])
+        self.btn_7 = tk.Button(self.buttons_frame, text=7, cnf=self.settings["buttons"]["numbers"])
+        self.btn_8 = tk.Button(self.buttons_frame, text=8, cnf=self.settings["buttons"]["numbers"])
+        self.btn_9 = tk.Button(self.buttons_frame, text=9, cnf=self.settings["buttons"]["numbers"])
         
-        # Adding the buttons
+
+        # Creating the calculator operations 
+        self.settings["buttons"]["operations"].update(self.settings["buttons"]["global"])
+
+
+        # First row
         self.btn_0.grid(row=0, column=0, padx=1, pady=1)
         self.btn_1.grid(row=0, column=1, padx=1, pady=1)
+        self.btn_2.grid(row=0, column=2, padx=1, pady=1)
+        self.btn_3.grid(row=0, column=3, padx=1, pady=1)
+        # Second row
+        self.btn_4.grid(row=1, column=0, padx=1, pady=1)
+        self.btn_5.grid(row=1, column=1, padx=1, pady=1)
+        self.btn_6.grid(row=1, column=2, padx=1, pady=1)
+        self.btn_7.grid(row=1, column=3, padx=1, pady=1)
+        # Third row
+        self.btn_8.grid(row=2, column=0, padx=1, pady=1)
+        self.btn_9.grid(row=2, column=1, padx=1, pady=1)
         
 
     def start(self):
